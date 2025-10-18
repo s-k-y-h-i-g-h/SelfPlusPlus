e09983b38f98ace2ade913ad2b038601e3628a3b
skyhigh <skyhiigh@proton.me>
2025-10-18 10:58:09 +0100

    Added license info to help header in sppcli

--- src/SelfPlusPlusCLI/Common/SelfPlusPlusHelpProvider.cs
+++ src/SelfPlusPlusCLI/Common/SelfPlusPlusHelpProvider.cs
@@ -15,14 +15,13 @@ class SelfPlusPlusHelpProvider : HelpProvider
     // App-level help header (shown for `--help` without a command)
     public override IEnumerable<IRenderable> GetHeader(ICommandModel model, ICommandInfo? command)
     {
-        yield return new Markup(":chart_increasing:[bold]SelfPlusPlusCLI[/] [italic]v0.1[/]\n");
-        yield return new Align(
-            new Markup("[italic]You plus a bit more![/] [bold][/]\n"), 
-            HorizontalAlignment.Left
-        );
+        yield return new Markup(":chart_increasing:[bold]SelfPlusPlusCLI[/] [italic]v0.1[/]\n\n");
+
         yield return new Markup("ABOUT:\n");
         yield return new Markup("    Known as [bold]SelfPlusPlus[/], [bold]Self++[/] and [bold]spp[/]\n\n");
-        yield return new Markup("    :copyright: George Zankevich 2025\n\n");
+        yield return new Markup("    [dim]Released under the GNU GPL v3.0 License[/]\n");
+        yield return new Markup("    [dim]See LICENSE file for full license text[/]\n");
+        yield return new Markup("    [dim]:copyright: George Zankevich 2025[/]\n\n");
     }
 
 }
